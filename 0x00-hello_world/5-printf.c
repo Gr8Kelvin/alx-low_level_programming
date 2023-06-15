4ff745614f091eba96c46518af4fe4dfcfd30bb1
Gr8kelvin <gr8kelvin@gmail.com>
2023-06-15 10:35:36 -0700

    0x00. C-Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<studio.h>
+/**
+ * main - Entry point
+ *
+ * Rwturn: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
