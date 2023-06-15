3d9161148cf81234006c0deeb93cdc3e10fb84ef
Gr8kelvin <gr8kelvin@gmail.com>
2023-06-15 12:12:31 -0700

    0x00. Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
-#!/bin/bash
 #include<studio.h>
 /**
  * main - Entry point
  *
- * Rwturn: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -11,6 +10,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long long int %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return 0;
+	return (0);
 
 }
