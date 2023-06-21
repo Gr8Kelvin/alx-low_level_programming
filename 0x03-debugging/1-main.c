7214b81d7d68f192d453cad9d3c7088e2330f676
Gr8kelvin <gr8kelvin@gmail.com>
2023-06-21 05:10:38 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes loop
- * Return 0
+ * Return: 0
  */
 
 int main(void)
@@ -14,10 +14,10 @@ int main(void)
 	i = 0;
 	/**
 	 * while (i < 10)
-	*{
-	* putchar(i);
-	*}
-	*/
+	 * {
+	 *	putchar(i);
+	 * }
+	 */
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
