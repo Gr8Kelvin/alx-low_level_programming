41f4f1e140d7002c076061d04316ba3ddf7a82f2
Gr8kelvin <gr8kelvin@gmail.com>
2023-06-22 13:04:39 -0700

    More functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Check for uppercase
+ * @c: The character
+ * Return: 1 uppercase
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 9)
+	{return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies
+ * @a - first
+ * @b - second
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+	return (result);
+}
