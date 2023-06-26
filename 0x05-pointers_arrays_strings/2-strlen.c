be8bef57658742bf14d0492ce8a9f54a7a329079
Gr8kelvin <gr8kelvin@gmail.com>
2023-06-26 04:35:47 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Checks length
+ * @s: string
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int string_len = 0;
+
+	while(s[string_len])
+		string_len++;
+	return(string_len);
+}
